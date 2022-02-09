9467c9f0cb8c8b657252892d3a91e048230668fb
Darren Snell <darren_snell@hotmail.co.uk>
2022-02-09 09:13:05 +0100

    Index.js chan ged

--- my-app/index.js
+++ my-app/index.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function getRandomCocktail () {
+    //instead of the url here i actuailly need it to be "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + ingredient search
     fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
   .then(
     function(response) {
