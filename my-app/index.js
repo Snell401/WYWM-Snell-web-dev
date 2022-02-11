7ccb94efbad35fd8f6efc53f3a793ab782379fe0
Darren Snell <85729783+Snell401@users.noreply.github.com>
2022-02-11 12:09:35 +0100

    Updated ingredients

--- my-app/index.js
+++ my-app/index.js
@@ -1,174 +1,167 @@
-// console.log('Hello World');
+//*************connect html elements to js
+const title = document.getElementById('title');
+const search = document.getElementById('findByIngredient');
+const ingredient = document.getElementById('cocktailIngredient');
+const getRandom = document.getElementById('randomButton');
+const drinkSection = document.getElementById('drink-section');
 
-/**
- * If you place your <script> tag at the end of the body, or if you
- * place it in the <head> tag, with the attribute `defer`, you can be
- * sure that your script will not run until the DOM content is loaded,
- * so you won't need to listen for a "DOMContentLoaded" event
- */
-// document.addEventListener('DOMContentLoaded', function() {
-/**
- * You need to apply the `addEventListener` method to an HTML element
- */
-//     addEventListener('click', function() {
-/**
- * I recommend using an `id` and `getElementById` rather than
- * `querySelector`, since this latter might not return the element
- * that you expect, if someone else rearranges your HTML
- */
-//         document.querySelector('.center').innerHTML = 'label';
-//
-// });
-// });
+// name the URLs here to make it cleaner in the functions
+const randomURL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+const ingredientURL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+const idURL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
-const title = document.getElementById("title");
-const search = document.getElementById("findByIngredient");
-const getRandom = document.getElementById("randomButton");
-const drinkSection = document.getElementById("drink-section");
-
-const randomURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-const ingredientURL =
-  "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=";
+// UTILITY FUNCTIONS // UTILITY FUNCTIONS // UTILITY FUNCTIONS //
 
 
-
-// RANDOM COCKTAIL // RANDOM COCKTAIL // RANDOM // RANDOM COCKTAIL //
-
-function getRandomCocktail() {
-  fetch(randomURL)
-  .then(treatResponse)
-  .catch(function (err) {
-    console.log("Fetch Error :-S", err);
-  });
-
-  function treatResponse(response) {
+function checkStatus(response) {
     if (response.status !== 200) {
-      console.log(
-        "Looks like there was a problem. Status Code: " + response.status
-      );
-      return;
+      throw new Error(`Status error: ${response.status}`)
+  
+    } else {
+      return response.json()
     }
-
-    // Examine the text in the response
-    response.json()
-    .then(replaceCurrentCocktail);
   }
-
-  function replaceCurrentCocktail(data) {
+  
+  
+  function clearBar() {
     // Remove current children of drink section
     while (drinkSection.firstChild) {
       drinkSection.removeChild(drinkSection.lastChild);
     }
-
+  }
+  
+  
+  function addCocktail(data) {
     // data will have the structure { drinks: Object[] }
-    // Only one cocktail will be returned, so we simply choose the
+    // Only one cocktail will be present, so we simply choose the
     // first object in the drinks array
     displayCocktail(data.drinks[0]);
   }
+
+
+
+  // RANDOM COCKTAIL // RANDOM COCKTAIL // RANDOM // RANDOM COCKTAIL //
+
+
+//*************get the API and iterate through the objects to display the results if fail then catch it and display the error********
+function getRandomCocktail() {
+    fetch(randomURL)
+    .then(checkStatus)
+    .then(replaceCurrentCocktail)
+        .catch(function (err) {
+            console.log('There was an error:', err);
+        });
+    // ****** clear the bar before adding the new cocktail *********
+    function replaceCurrentCocktail(data) {
+        clearBar ()
+        addCocktail(data)
+    }
 }
 
-getRandomCocktail();
+// FIND BY INGREDIENT // FIND BY INGREDIENT // FIND BY INGREDIENT //
+
+function findByIngredient() {
+    const chosenIngredient = ingredient.value
+    if (!chosenIngredient) {
+      return
+    } else if (chosenIngredient.indexOf(",") > -1) {
+      // You need to be a $2+ Patreon supporter to search for multi-ingredients so i can only show you the first one
+      return
+    }
+    
+    // ********* replace the current cocktail with the new one *********
+   const searchURL = `${ingredientURL}${chosenIngredient}`;
+    fetch(searchURL)
+    .then(checkStatus)
+    .then(replaceCurrentCocktails)
+        .catch(function (err) {
+            console.log('There was an error:', err);
+        });
+ /**
+   *
+   * @param {Object} data has the format:
+   * { "drinks": [
+   *     {
+   *       "strDrink": "Brandy Flip",
+   *       "strDrinkThumb": "https://www.thecocktaildb.com/images/...jpg",
+   *       "idDrink": "11164"
+   *     },
+   *     ...
+   *   ]
+   * }
+   */
+
+  function replaceCurrentCocktails(data) {
+    // console.log("data", JSON.stringify(data, null, '  '));
+    clearBar()
+    data.drinks.forEach(getCocktailById);
+  }
+  function getCocktailById(cocktailData) {
+    const id = cocktailData.idDrink
+    const searchURL = `${idURL}${id}`
+
+    fetch(searchURL)
+    .then(checkStatus)
+    .then(addCocktail)
+    .catch(error => console.log("Error in getCocktailById", error));
+  }
+}
+
 
 // DISPLAY COCKTAIL // DISPLAY COCKTAIL / DISPLAY / DISPLAY COCKTAIL //
 
-/**
- * Creates a set of DOM elements to insert into section#drink-section.
- * Example:
- * <section id="drink-section">
- *   <h2>Rum Cobbler</h2>
- *   <img src="https://www.thecocktaildb.com/images/...jpg">
- *   <ul>Sugar: 1 tsp superfine </ul>
- *   <ul>Club soda: 3 oz </ul>
- *   <ul>Lemon: 1 </ul>
- *   <ul>Dark rum: 2 oz </ul>
- *   <ul>Maraschino cherry: 1 </ul>
- *   <ul>Orange: 1 </ul>
- *   <p>In an old-fashioned glass... and lemon slices.</p>
- * </section>
- *
- * @param {Object} cocktail. For example:
- * {
- *   "idDrink": "17006",
- *   "strDrink": "Kool First Aid",
- *   "strDrinkAlternate": null,
- *   "strTags": null,
- *   "strVideo": null,
- *   "strCategory": "Shot",
- *   "strIBA": null,
- *   "strAlcoholic": "Alcoholic",
- *   "strGlass": "Shot glass",
- *   "strInstructions": "Add Kool Aid ... Slam and shoot.",
- *   "strInstructionsES": null,
- *   "strInstructionsDE": "Geben Sie Kool Aid... und Schuss.",
- *   "strInstructionsFR": null,
- *   "strInstructionsIT": "Aggiungi Kool-Aid... il rum.",
- *   "strInstructionsZH-HANS": null,
- *   "strInstructionsZH-HANT": null,
- *   "strDrinkThumb": "https://www.thecocktaildb.com/...jpg",
- *   "strIngredient1": "151 proof rum",
- *   "strIngredient2": "Kool-Aid",
- *   ...
- *   "strIngredient15": null,
- *   "strMeasure1": "2 oz light ",
- *   "strMeasure2": "1/2 tsp Tropical ",
- *   ...
- *   "strMeasure15": null,
- *   "strImageSource": null,
- *   "strImageAttribution": null,
- *   "strCreativeCommonsConfirmed": "No",
- *   "dateModified": "2017-08-24 09:53:44"
- * }
- */
+// ***** display the information about the cocktail
 function displayCocktail(cocktail) {
- // console.log("cocktail", JSON.stringify(cocktail, null, '  '));
+    //console.log("cocktail", JSONstringify(cocktail, null,' '));
+    // Add h2 title with name of the cocktail
+    const drinkName = document.createElement('h2');
+    drinkName.innerHTML = cocktail.strDrink;
+    drinkSection.appendChild(drinkName);
+    //add image of the cocktail
+    const img = document.createElement('img');
+    img.src = cocktail.strDrinkThumb;
+    drinkSection.appendChild(img);
+    // add <ul> with ingredients
+    const ul = document.createElement('ul');
+    drinkSection.appendChild(ul)
 
-  // Add an h2 title with the name of the cocktail
-  const drinkName = document.createElement("h2");
-  drinkName.innerHTML = cocktail.strDrink;
-  drinkSection.appendChild(drinkName);
-
-  // Add the image for the cocktail
-  const img = document.createElement("img");
-  img.src = cocktail.strDrinkThumb;
-  drinkSection.appendChild(img);
-
-  // Add <ul> for ingredients list
-  const ul = document.createElement("ul")
-  drinkSection.appendChild(ul)
-
-  // Add <li> element for each ingredient
-  for (let i = 1; i < 16; i++) {
-    // If there aren't any more ingredients, the list is complete
-    const ingredient = cocktail[`strIngredient${i}`]
-    if (!ingredient) {
-      break;
+// ******* there are  UPTO 15 ingredients in the cocktail therefor loop through 15 times only *********
+    for (let i = 1; i < 16; i++) {
+        //if there are no more ingredients, break out of the loop
+        const ingredient = cocktail[`strIngredient${i}`];
+        if (!ingredient) {
+            break;
+        }
+        // Some ingredients like "egg" don't have any quanity, so we need
+        // to account for that
+        let quantity = cocktail[`strMeasure${i}`] || "";
+        if (quantity) {
+            quantity = ": " + quantity
+        }
+        // create a <li> for each ingredient and the quantity and append it to the <ul>
+        const ingredientLI = document.createElement("li");
+        ingredientLI.innerText = ingredient + quantity;
+        ul.appendChild(ingredientLI);
     }
-
-    // Some ingredients like "egg" don't have any quanity, so we need
-    // to account for that
-    let quantity = cocktail[`strMeasure${i}`] || "";
-    if (quantity) {
-      quantity = ": " + quantity
-    }
-    const ingredientLI = document.createElement("li");
-    ingredientLI.innerText = ingredient + quantity;
-
-    ul.appendChild(ingredientLI);
-  }
-
-  let instructions = document.createElement("p");
-  instructions.innerHTML = cocktail.strInstructions;
-  drinkSection.appendChild(instructions);
+//  ******* add the instructions to the page *********
+    let instructions = document.createElement("p");
+    instructions.innerHTML = cocktail.strInstructions;
+    drinkSection.appendChild(instructions);
 }
 
-/**
- * You would need to appendChild() this newButton element somewhere
- */
-// //newButton to refresh the page
-// let newButton = document.createElement("button");
-// newButton.innerHTML = "New Cocktail";
-// newButton.addEventListener("click", function () {
-//   location.reload();
-// });
+// KEYBOARD SHORTCUT // KEYBOARD SHORTCUT // KEYBOARD SHORTCUT //
 
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+      event.preventDefault()
+      findByIngredient()
+    }
+  }
+  
+  // Activate the buttons
+  search.addEventListener("click", findByIngredient)
+  ingredient.addEventListener("keyup", checkForEnter)
+
+  // Open the page with a random cocktail by default
+getRandomCocktail();
 getRandom.addEventListener("click", getRandomCocktail)
